e7e0eb7dd685f646eb2e76d8dddaac1a22b2a7af
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-11-01 11:39:12 -0300

    fintow

--- app.js
+++ app.js
@@ -14,7 +14,12 @@ function criaCartao(categoria, pergunta, resposta) {
     </div>
     </div>
     `
-
+    let respostaEstaVisivel = false
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+    cartao.addEventListener('click', viraCartao)
     container.appendChild(cartao)
 
     cartao.innerHTML = `
