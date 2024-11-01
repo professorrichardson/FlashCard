41168eef6b17e3d0699d85cb977ea49b1db3c0e1
Richardson Schawarski Cruz <125823124+professorrichardson@users.noreply.github.com>
2024-11-01 11:35:31 -0300

    fixone

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
-function criaCartao (categoria, pergunta, resposta) {
-    console.log(categoria, pergunta, resposta)
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
+
+    cartao.innerHTML = `
+    <div class="cartao__conteudo">
+    <h3>Programação</h3>
+    <div class="cartao__conteudo__pergunta">
+            <p>O que é JavaScript?</p>
+    </div>
+    <div class="cartao__conteudo__resposta">
+            <p>O JavaScript é uma linguagem de programação</p>
+    </div>
+    </div>
+    `
+
+    container.appendChild(cartao)
+
+    cartao.innerHTML = `
+        <div class="cartao__conteudo">
+        <h3>${categoria}</h3>
+        <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
+        </div>
+        <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+        </div>
+        </div>
+`
 }
